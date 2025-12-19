eb630ee4ed53f24782e46fec008970efcb36c474
Tony Brobston <tony@Mac.localdomain>
2025-12-19 11:46:10 -0600

    Update base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: "/income-conversion-calculator",
   images: {
     unoptimized: true,
   },
